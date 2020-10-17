29e74527fd221157bec1d25ec571dd8c648bbcc6
Gal Itach <itachgal@gmail.com>
2020-10-17 13:57:45 +0300

    DMApp - Sidebar related components

---
+++ src/components/global/Sidebar.js
@@ -0,0 +1,43 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { MoreVert, Chat, DonutLarge, SearchOutlined } from '@material-ui/icons'
+import React from 'react'
+import './Sidebar.css'
+import SidebarChat from './SidebarChat'
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLarge/>
+                    </IconButton>
+
+                    <IconButton>
+                        <Chat/>
+                    </IconButton>
+                    
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                    
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or Start Chat" type="text"/>
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCujl1yv46zuMebHe46M2JIowMcUrEBDuI",
+    authDomain: "dmapp-668a2.firebaseapp.com",
+    databaseURL: "https://dmapp-668a2.firebaseio.com",
+    projectId: "dmapp-668a2",
+    storageBucket: "dmapp-668a2.appspot.com",
+    messagingSenderId: "1036871886587",
+    appId: "1:1036871886587:web:c16824383d1c686c4af25e"
+  };
